f75f7b698c951ab2c00b78526c2012e42c2c6fe5
robertooliveiraoficial <roberto.oliveira.oficial@outlook.com>
2023-07-07 15:20:05 -0400

    Refatoração da BookController.

--- Projeto API - Book Manager/BooksManager/Controllers/BookController.cs
+++ Projeto API - Book Manager/BooksManager/Controllers/BookController.cs
@@ -4,63 +4,57 @@ using System.Text.Json;
 
 namespace BooksManager.Controllers;
 
+/// <summary>
+/// BookController
+/// </summary>
 [ApiController]
 [Route("[controller]")]
 public class BookController : ControllerBase
 {
-    #region Declarações
-    private static List<Book> books = new List<Book>();
-    private static string urlJson = "https://raw.githubusercontent.com/timeiagro/BackendProvaConceitoTimeIAGRO/main/books.json";
+    #region Statements
+    private static IEnumerable<Book> books = new List<Book>();
+    private static string urlJsonFile = "https://raw.githubusercontent.com/timeiagro/BackendProvaConceitoTimeIAGRO/main/books.json";
     #endregion
 
     /// <summary>
     /// - Retorna uma lista de todos os livros
     /// </summary>
-    /// <param name="No Params">No Params</param>
     /// <returns>Json</returns>
     /// <response code="200">Caso a consulta seja executada com sucesso</response>
-    [HttpGet("GetBooks")]
+    [HttpGet("/api/books")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public async Task<List<Book>> JsonForBooks()
-    {
-        var getJson = string.Empty;
-        var getJsonFile = await new HttpClient().GetStringAsync(urlJson);
-        books = JsonSerializer.Deserialize<List<Book>>(getJsonFile)!;
+    public async Task<IEnumerable<Book>> JsonForBooks()
+    {        
+        var getJsonFile = await new HttpClient().GetStringAsync(urlJsonFile);
+        books = JsonSerializer.Deserialize<IEnumerable<Book>>(getJsonFile)!;
         return books;
     }
-
+    
     /// <summary>
     /// - Retorna uma lista de todos os livros em ordem CRESCENTE tendo o valor do campo "preço" usado como referência/parâmetro
     /// </summary>
-    /// <param name="No Params">No Params</param>
     /// <returns>Json</returns>
     /// <response code="200">Caso a consulta seja executada com sucesso</response>
-    [HttpGet("OrderByPriceAsc")]
+    [HttpGet("/api/books/price/asc")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public async Task<List<Book>> GetBooksOrderByPriceAsc()
-    {
-        var getJson = string.Empty;
-        var getJsonFile = await new HttpClient().GetStringAsync(urlJson);
-
-        books = JsonSerializer.Deserialize<List<Book>>(getJsonFile)!;
-        
+    public async Task<IEnumerable<Book>> GetBooksOrderByPriceAsc()
+    {        
+        var getJsonFile = await new HttpClient().GetStringAsync(urlJsonFile);
+        books = JsonSerializer.Deserialize<IEnumerable<Book>>(getJsonFile)!;        
         return books.OrderBy(b => b.Preco).ToList();
     }
 
     /// <summary>
     /// - Retorna uma lista de todos os livros em ordem DECRESCENTE tendo o valor do campo "preço" usado como referência/parâmetro
     /// </summary>
-    /// <param name="No Params">No Params</param>
     /// <returns>Json</returns>
     /// <response code="200">Caso a consulta seja executada com sucesso</response>
-    [HttpGet("OrderByPriceDesc")]
+    [HttpGet("/api/books/price/desc")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public async Task<List<Book>> GetBooksOrderByPriceDesc()
-    {
-        var getJson = string.Empty;
-        var getJsonFile = await new HttpClient().GetStringAsync(urlJson);
-
-        books = JsonSerializer.Deserialize<List<Book>>(getJsonFile)!;
+    public async Task<IEnumerable<Book>> GetBooksOrderByPriceDesc()
+    {        
+        var getJsonFile = await new HttpClient().GetStringAsync(urlJsonFile);
+        books = JsonSerializer.Deserialize<IEnumerable<Book>>(getJsonFile)!;
         return books.OrderByDescending(b => b.Preco).ToList();            
     }
 
@@ -70,34 +64,28 @@ public class BookController : ControllerBase
     /// <param name="author">Nome do autor do livro</param>
     /// <returns>Json</returns>
     /// <response code="200">Caso a consulta seja executada com sucesso</response>
-    [HttpGet("GetBooksByAuthorName")]
+    [HttpGet("/api/books/author")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public async Task<List<Book>> GetBooksByAuthorName(string author)
-    {
-        var getJson = string.Empty;
-        var getJsonFile = await new HttpClient().GetStringAsync(urlJson);
-
-        books = JsonSerializer.Deserialize<List<Book>>(getJsonFile)!;
-
+    public async Task<IEnumerable<Book>> GetBooksByAuthorName(string author)
+    {        
+        var getJsonFile = await new HttpClient().GetStringAsync(urlJsonFile);
+        books = JsonSerializer.Deserialize<IEnumerable<Book>>(getJsonFile)!;
         return books.Where(b => b.Especificacoes!.Autor!.Contains(author, StringComparison.OrdinalIgnoreCase)).ToList();
     }
 
     /// <summary>
     /// - Retorna uma lista de todos os livros de acordo com o "nome do livro" passado como referência/parâmetro
     /// </summary>
-    /// <param name="bookName">Nome do livro</param>
+    /// <param name="name">Nome do livro</param>
     /// <returns>Json</returns>
     /// <response code="200">Caso a consulta seja executada com sucesso</response>
-    [HttpGet("GetBooksByBookName")]
+    [HttpGet("/api/books/name")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public async Task<List<Book>> GetBooksByBookName(string bookName)
-    {
-        var getJson = string.Empty;
-        var getJsonFile = await new HttpClient().GetStringAsync(urlJson);
-
-        books = JsonSerializer.Deserialize<List<Book>>(getJsonFile)!;
-
-        return books.Where(b => b.Nome!.Contains(bookName, StringComparison.OrdinalIgnoreCase)).ToList();
+    public async Task<IEnumerable<Book>> GetBooksByBookName(string name)
+    {        
+        var getJsonFile = await new HttpClient().GetStringAsync(urlJsonFile);
+        books = JsonSerializer.Deserialize<IEnumerable<Book>>(getJsonFile)!;
+        return books.Where(b => b.Nome!.Contains(name, StringComparison.OrdinalIgnoreCase)).ToList();
     }
 
     /// <summary>
@@ -106,15 +94,12 @@ public class BookController : ControllerBase
     /// <param name="genre">Gênero do livro</param>
     /// <returns>Json</returns>
     /// <response code="200">Caso a consulta seja executada com sucesso</response>
-    [HttpGet("GetBooksByGenre")]
+    [HttpGet("/api/books/genre")]
     [ProducesResponseType(StatusCodes.Status200OK)]
-    public async Task<List<Book>> GetBooksByGenre(string genre)
-    {
-        var getJson = string.Empty;
-        var getJsonFile = await new HttpClient().GetStringAsync(urlJson);
-
-        books = JsonSerializer.Deserialize<List<Book>>(getJsonFile)!;
-
+    public async Task<IEnumerable<Book>> GetBooksByGenre(string genre)
+    {        
+        var getJsonFile = await new HttpClient().GetStringAsync(urlJsonFile);
+        books = JsonSerializer.Deserialize<IEnumerable<Book>>(getJsonFile)!;
         return books.Where(b => b.Especificacoes!.Genres!.ToString()!.Contains(genre, StringComparison.OrdinalIgnoreCase)).ToList();
-    }    
+    } 
 }
